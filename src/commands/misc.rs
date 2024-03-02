8fe9922732043c347c9bdd1545ebf3ea2ad3f7b4
Gabriel Rosa <gabr1sr@proton.me>
2024-03-01 23:17:44 -0300

    format: all code

--- src/commands/misc.rs
+++ src/commands/misc.rs
@@ -13,7 +13,8 @@ pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
 pub async fn database(ctx: Context<'_>) -> Result<(), Error> {
     let row: (i64,) = sqlx::query_as("SELECT $1")
         .bind(150_i64)
-        .fetch_one(&ctx.data().database.pool).await?;
+        .fetch_one(&ctx.data().database.pool)
+        .await?;
 
     let res = format!("Value: {}", row.0);
     ctx.reply(res).await?;
