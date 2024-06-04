1128b5f49951f78c7ccb9f6e2d10509c26100c47
Gabriel Rosa <gabr1sr@proton.me>
2024-06-04 12:53:56 -0300

    improve: untimeout command checks and response
    Closes #80

--- src/commands/moderation.rs
+++ src/commands/moderation.rs
@@ -166,9 +166,13 @@ pub async fn timeout(
 )]
 pub async fn untimeout(ctx: Context<'_>, users: String) -> Result<(), Error> {
     ctx.defer_ephemeral().await?;
-    let users_str = users.as_str();
-    let mut user_ids: Vec<UserId> = user_ids_from(users_str);
-    let guild_id = ctx.guild_id().unwrap();
+    let mut user_ids: Vec<UserId> = user_ids_from(&users);
+
+    if user_ids.is_empty() {
+        ctx.reply("You must provide at least 1 valid user mention or user ID.")
+            .await?;
+        return Ok(());
+    }
 
     if !assert_highest_role(&ctx, &mut user_ids).await.unwrap() {
         ctx.reply("One of the users have a role higher than yours.")
@@ -176,9 +180,32 @@ pub async fn untimeout(ctx: Context<'_>, users: String) -> Result<(), Error> {
         return Ok(());
     }
 
-    let result = untimeout_users(ctx, guild_id, &mut user_ids).await?;
-    let res = unpunish_response(result);
-    ctx.reply(res).await?;
+    let guild_id = ctx.guild_id().unwrap();
+    let (unpunished_users, not_unpunished_users) =
+        untimeout_users(ctx, guild_id, &mut user_ids).await?;
+
+    let mut message = String::new();
+
+    if !unpunished_users.is_empty() {
+        let mentions = user_ids_to_mentions(unpunished_users).join(", ");
+        let res = format!(
+            ":white_check_mark: **Successfully untimedout members:** {}\n",
+            mentions
+        );
+        message.push_str(&res);
+    }
+
+    if !not_unpunished_users.is_empty() {
+        let mentions = user_ids_to_mentions(not_unpunished_users).join(", ");
+        let res = format!(":warning: **Failed to untimeout members:** {}\n", mentions);
+        message.push_str(&res);
+    }
+
+    if message.is_empty() {
+        message.push_str("Failed to execute untimeout command!");
+    }
+
+    ctx.reply(message).await?;
     Ok(())
 }
 
@@ -656,19 +683,6 @@ fn punish_response((punished_users, not_punished_users): (Vec<UserId>, Vec<UserI
         "Punished users: {}\nNot punished users: {}",
         punished_mentions.join(", "),
         not_punished_mentions.join(", ")
-    )
-}
-
-fn unpunish_response(
-    (unpunished_users, not_unpunished_users): (Vec<UserId>, Vec<UserId>),
-) -> String {
-    let unpunished_mentions = user_ids_to_mentions(unpunished_users);
-    let not_unpunished_mentions = user_ids_to_mentions(not_unpunished_users);
-
-    format!(
-        "Unpunished users: {}\nNot unpunished users: {}",
-        unpunished_mentions.join(", "),
-        not_unpunished_mentions.join(", ")
     )
 }
 
