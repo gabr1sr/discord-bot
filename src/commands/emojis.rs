2bb58605e0be7895513b6b7161b5db2975866fcf
Gabriel Rosa <gabr1sr@proton.me>
2024-06-24 16:03:44 -0300

    rewrite: emoji add command

--- src/commands/emojis.rs
+++ src/commands/emojis.rs
@@ -1,9 +1,10 @@
 use crate::{Context, Error};
 use poise::serenity_prelude::all::Emoji;
+use serenity::all::{Attachment, CreateAttachment};
 
 #[poise::command(
     slash_command,
-    subcommands("show"),
+    subcommands("show", "add"),
     subcommand_required,
     category = "Emojis"
 )]
@@ -11,9 +12,32 @@ pub async fn emoji(_: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
 
-#[poise::command(slash_command, required_permissions = "SEND_MESSAGES", guild_only)]
+#[poise::command(slash_command, required_bot_permissions = "SEND_MESSAGES", guild_only)]
 pub async fn show(ctx: Context<'_>, emoji: Emoji) -> Result<(), Error> {
-    let url = format!("{}?size=2048", emoji.url());
-    ctx.reply(url).await?;
+    ctx.reply(format!("{}?size=2048", emoji.url())).await?;
+    Ok(())
+}
+
+#[poise::command(
+    slash_command,
+    required_bot_permissions = "CREATE_GUILD_EXPRESSIONS",
+    required_permissions = "CREATE_GUILD_EXPRESSIONS",
+    guild_only
+)]
+pub async fn add(ctx: Context<'_>, name: String, attachment: Attachment) -> Result<(), Error> {
+    let bytes = attachment.download().await?;
+    let builder = CreateAttachment::bytes(bytes, &name);
+
+    let res = match ctx
+        .guild_id()
+        .unwrap()
+        .create_emoji(&ctx, &name, &builder.to_base64())
+        .await
+    {
+        Err(error) => format!(":x: Failed to create emoji `{name}`: {:?}", dbg!(error)),
+        Ok(emoji) => format!(":white_check_mark: Emoji `{name}` created with success: {emoji}"),
+    };
+
+    ctx.reply(res).await?;
     Ok(())
 }
