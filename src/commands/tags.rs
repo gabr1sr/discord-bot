84cb47e7536f6b2ff6db5f121ddc4e2caedfda93
Gabriel Rosa <gabr1sr@proton.me>
2024-06-26 02:18:07 -0300

    rewrite: tag remove command

--- src/commands/tags.rs
+++ src/commands/tags.rs
@@ -5,7 +5,7 @@ use sqlx::{Pool, Postgres};
 
 #[poise::command(
     slash_command,
-    subcommands("add", "show", "edit"),
+    subcommands("add", "show", "edit", "remove"),
     subcommand_required,
     category = "Tags"
 )]
@@ -54,6 +54,22 @@ pub async fn edit(ctx: Context<'_>, name: String, content: String) -> Result<(),
     Ok(())
 }
 
+#[poise::command(slash_command)]
+pub async fn remove(ctx: Context<'_>, name: String) -> Result<(), Error> {
+    ctx.defer_ephemeral().await?;
+    let builder = CreateTag::new(&name, "", ctx.author());
+
+    let res = match delete_tag(&ctx.data().pool, builder).await {
+        Err(_) | Ok(0) => {
+            format!(":x: You're not the owner of the tag `{name}` or it doesn't exist!")
+        }
+        Ok(_) => format!(":white_check_mark: Tag `{name}` deleted with success!"),
+    };
+
+    ctx.reply(res).await?;
+    Ok(())
+}
+
 async fn create_tag(pool: &Pool<Postgres>, builder: CreateTag) -> Result<Tag, sqlx::Error> {
     sqlx::query_as!(
         Tag,
@@ -82,4 +98,15 @@ async fn update_tag(pool: &Pool<Postgres>, builder: CreateTag) -> Result<Tag, sq
     )
     .fetch_one(pool)
     .await
+}
+
+async fn delete_tag(pool: &Pool<Postgres>, builder: CreateTag) -> Result<u64, sqlx::Error> {
+    Ok(sqlx::query!(
+        r#"DELETE FROM tags WHERE name = $1 AND owner = $2"#,
+        builder.name,
+        builder.owner.get().to_string()
+    )
+    .execute(pool)
+    .await?
+    .rows_affected())
 }
